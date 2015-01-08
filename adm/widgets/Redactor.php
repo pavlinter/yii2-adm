4f310dca383de4188c63d73f609bbf80f5b331d0
Pavel <pavlinter@gmail.com>
2015-01-08 22:07:00 +0200

    Redactor init on focus textarea

--- adm/widgets/Redactor.php
+++ adm/widgets/Redactor.php
@@ -5,9 +5,11 @@ namespace pavlinter\adm\widgets;
 use mihaildev\elfinder\ElFinder;
 use pavlinter\adm\Adm;
 use Yii;
-use mihaildev\ckeditor\CKEditor;
 use yii\widgets\InputWidget;
 
+/**
+ * Class Redactor
+ */
 class Redactor extends InputWidget
 {
     /**
@@ -20,9 +22,17 @@ class Redactor extends InputWidget
         parent:: init();
     }
 
+    /**
+     * Executes the widget.
+     * @return string the result of widget execution to be outputted.
+     */
     public function run()
     {
         return $this->form->field($this->model, $this->attribute)->widget(CKEditor::className(), [
+            'initOnEvent' => 'focus',
+            'options' => [
+                'class' => 'form-control form-redactor',
+            ],
             'editorOptions' => ElFinder::ckeditorOptions(Adm::getInstance()->id.'/elfinder',[/* Some CKEditor Options */]),
         ]);
     }
