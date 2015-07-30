2944f2c8c971294b8f693b74a51726733830d67d
Pavel <pavlinter@gmail.com>
2015-07-30 19:47:09 +0300

    version 1.0.4

--- adm/assets/js/nestable/jquery.nestable.js
+++ adm/assets/js/nestable/jquery.nestable.js
@@ -2,7 +2,7 @@
  * @package yii2-adm
  * @author Pavels Radajevs <pavlinter@gmail.com>
  * @copyright Copyright &copy; Pavels Radajevs <pavlinter@gmail.com>, 2015
- * @version 1.0.3
+ * @version 1.0.4
  *
  * Nestable only for adm module
  * Nestable jQuery Plugin - http://dbushell.com
