afd1bc236637c4f8f9e6d96e55e81f4cb30c81c0
pavlinter <pavlinter@gmail.com>
2015-10-23 12:29:31 +0300

    add priview

--- adm/widgets/FileInput.php
+++ adm/widgets/FileInput.php
@@ -14,6 +14,7 @@ use mihaildev\elfinder\InputFile;
 use pavlinter\adm\Adm;
 use Yii;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 use yii\widgets\InputWidget;
 
 /**
@@ -28,21 +29,51 @@ class FileInput extends InputWidget
 
     public $fileManager = [];
 
+    public $enablePreview = true;
+
+    public $basePath = '@web';
+
     public function init()
     {
+        $id = $this->getId();
+        $buttonId = 'fm-btn-' . $id;
         $this->fileManager = ArrayHelper::merge([
             'controller'    => Adm::getInstance()->id.'/elfinder',
             'filter'        => 'image', // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#wiki-onlyMimes
-            'template'      => '<div class="input-group"><span class="input-group-addon">{button}</span>{input}</div>',
+            'template'      => '<div class="input-group fm-input-cont"><span class="input-group-addon">{button}</span>{input}</div>',
             'options'       => ['class' => 'form-control'],
             'buttonTag'     => 'a',
             'buttonName'    => '',
             'buttonOptions' => [
                 'class' => 'glyphicon glyphicon-folder-open filemanager-btn',
-                'href'  => 'javascript:void(0);'
+                'href'  => 'javascript:void(0);',
+                'id'    => $buttonId,
+                'data' => [
+                    'placement' => 'top',
+                ],
             ],
             'multiple'      => true
         ], $this->fileManager);
+
+        if ($this->enablePreview) {
+            $basePath = Yii::getAlias($this->basePath);
+            $this->getView()->registerJs('
+            $("#' . $buttonId . '").tooltip({
+                html: true,
+                title: function(){
+                    var v = $(this).closest(".fm-input-cont").find("#' . Html::getInputId($this->model, $this->attribute) . '").val();
+                    var $img = $("<img src=\"' . $basePath . '" + v + "\" class=\"hide file-input-img\">");
+                    var $loading = $("<span class=\"file-input-loading fa fa-spinner rotating\"></span>");
+                    $img.on("load", function(){
+                        $img.removeClass("hide");
+                        $loading.hide();
+                    })
+                    return $("<div class=\"file-input-cont\"><div>").append($loading).append($img);
+                },
+            });
+        ');
+        }
+
 
         parent:: init();
     }
