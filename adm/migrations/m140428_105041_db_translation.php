c272950da0666bd95826ddc327a03410d457aaf3
Pavel <pavlinter@gmail.com>
2016-03-22 20:32:27 +0200

    @version 1.1.1

--- adm/migrations/m140428_105041_db_translation.php
+++ adm/migrations/m140428_105041_db_translation.php
@@ -4,7 +4,7 @@
  * @package yii2-adm
  * @author Pavels Radajevs <pavlinter@gmail.com>
  * @copyright Copyright &copy; Pavels Radajevs <pavlinter@gmail.com>, 2015
- * @version 1.1.0
+ * @version 1.1.1
  */
 
 use yii\db\Schema;
